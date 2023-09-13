86f335f748a2efab3052b397c14d8bc90d2af0e1
thao <thaole0325@gmail.com>
2023-09-13 17:36:29 -0400

    updated endpoint to handle range of words

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,20 @@ from scraper import scrape_words
 
 words_bp = Blueprint("words", __name__, url_prefix="/words")
 
-@words_bp.route("/<language>", methods=["GET"])
-def get_words(language):
+@words_bp.route("/<language>/<int:start>/<int:end>", methods=["GET"])
+def get_words(language, start, end):
+    """
+    Endpoint to fetch a range of words in a specified language using the scrape_words function
+    
+    Args:
+    language (str)
+    start (int), end (int): Ehe range of words to retrieve
+    
+    Returns:
+    JSON: list of words and details, or an error message
+    """
     try:
-        words = scrape_words(language)
+        words = scrape_words(language, start, end)
         return jsonify(words), 200
     except Exception as e: 
         # extract error message string from exception object
