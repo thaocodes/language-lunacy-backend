d0a4edf827b6fbbd1d50991dc26119c84460bead
thao <thaole0325@gmail.com>
2023-09-13 13:07:21 -0400

    python script for scraping webpage for words in language & english

---
+++ app/scraper.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup
+import requests
+
+def get_language_words(language):
+    '''
+    Fetches a list of common words in the specified language along with their English translations.
+    
+    Args: 
+    language (string): The target language for fetching the words.
+
+    Returns:
+    tuple: A JSON response containing the words list or an error message, and the HTTP status code.
+    '''
+    language = language.lower()
+    url = f'https://1000mostcommonwords.com/1000-most-common-{language}-words/'
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        raise Exception(f"No list of words for this language '{language.capitalize()}'")
+        
+    # parse response (webpage)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    words = []
+
+    # find all table rows, excluding header row
+    rows = soup.find_all('tr')[1:]
+    for row in rows:
+        columns = row.find_all('td')[1:3]    # get only the columns with words
+        if len(columns) == 2:                # ensure we have both words
+            words.append({
+                language: columns[0].text,    # first column is {language} word
+                "english": columns[1].text,   # second column is english translation 
+                "difficulty": "unset"
+            })
+            
+    return words
+            
+
+    
