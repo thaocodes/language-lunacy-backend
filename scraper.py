5788131d93efcb159e0380bc7e16c78f96d0ad44
thao <thaole0325@gmail.com>
2023-09-13 17:37:05 -0400

    updated web scraper script to handle a range of words

--- scraper.py
+++ scraper.py
@@ -2,17 +2,23 @@ from flask import Flask
 from bs4 import BeautifulSoup
 import requests
 
-def scrape_words(language):
+def scrape_words(language, start=0, end=100):
     '''
-    Fetches a list of common words in the specified language along with their English translations.
+    Fetches a subset of words in a specified language along with their English translations
     
     Args: 
-    language (string): The target language for fetching the words.
+    language (string): Target language for fetching words
+    start (integer): Starting index of the range (inclusive)
+    end (integer): Ending index of the range (exclusive)
 
     Returns:
-    words (array): A list of dictionary objects containing words in the specified language and English.
+    words (array): List of word dictionaries with word details
     '''
     language = language.lower()
+
+    if start < 0 or end > 1000 or start >= end: 
+        raise ValueError("Invalid start or end value. Valid range is 0-1000 and start should be less than end.")
+    
     url = f'https://1000mostcommonwords.com/1000-most-common-{language}-words/'
     response = requests.get(url)
 
@@ -22,16 +28,17 @@ def scrape_words(language):
     # parse response (webpage)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    words = []
-
     # find all table rows, excluding header row
     rows = soup.find_all('tr')[1:]
-    for row in rows:
-        columns = row.find_all('td')[1:3]    # get only the columns with words
-        if len(columns) == 2:                # ensure we have both words
+
+    words = []
+    for row in rows[start:end]:
+        columns = row.find_all('td')
+        if len(columns) == 3:
             words.append({
-                language: columns[0].text,    # first column is {language} word
-                "english": columns[1].text,   # second column is english translation 
+                "number": int(columns[0].text),
+                language: columns[1].text,
+                "english": columns[2].text,
                 "difficulty": "unset"
             })
 
