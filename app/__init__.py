47a6eb29236489782b13180c28a8b7a1d629b234
thao <thaole0325@gmail.com>
2023-09-15 12:26:40 -0400

    fixing cors blocked error from frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    CORS(app, origins=['http://localhost:3000'])
 
     from .routes import words_bp
     app.register_blueprint(words_bp)
